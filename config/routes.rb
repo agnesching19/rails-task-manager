be9714cf5178aaeee27133e97a45a1a47974004d
Agnes Ching <agnesching19@gmail.com>
2017-11-07 20:16:46 +0000

    made delete method work

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,11 @@
 Rails.application.routes.draw do
+  get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 
-  # get "tasks", to: "tasks#index", as: :tasks
-
-  # get "tasks/new", to: "tasks#new", as: :new_task
-
-  # get "tasks/:id", to: "tasks#show", as: :task
-
-  # post "tasks", to: "tasks#create"
-
-  # get "tasks/:id", to: "tasks#edit", as: :edit_task
-
-  # patch "tasks/:id", to: "tasks#update", as: :update_task
-
-
-  # delete "tasks/:id", to: "tasks#destroy", as: :delete_task
-
-  resources :tasks
-
+  # resources :tasks
 end
